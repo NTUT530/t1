20871e6143c07afcd7a2a9250b909069554f260b
Ntut530 <95620968+NTUT530@users.noreply.github.com>
2021-12-06 20:40:14 +0800

    Update t1.py

--- t1.py
+++ t1.py
@@ -1,5 +1,10 @@
 import streamlit as st
 
+page_bg_img="<style>
+body{
+background-image:url("https://i2.oubafeng.com/c310dd/984e8ba3/98168beb452e5e0fbe69.jpg");
+
+
 def hello(i,k):
   st.write((k+"\n")*i)
 
