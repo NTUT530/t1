59f7729a09afd6e8683d20d58c4ff09092a4e754
Ntut530 <95620968+NTUT530@users.noreply.github.com>
2021-12-07 18:29:21 +0800

    Update teat.py

--- teat.py
+++ teat.py
@@ -3,9 +3,9 @@ import streamlit as st
 
 colse_full_rice=["銀記","今華"]
 colse_full_noodle=["銀記"]
-colse_rice=["咖食堂","蔣老爹","永豆"]
+colse_rice=["咖食堂","蔣老爹","永和豆漿"]
 colse_noodle=["葛瑪莉","嵐迪","小四川"]
-colse_other=["麥當勞","永豆","煎餅果子","小四川"]
+colse_other=["麥當勞","永和豆漿","煎餅果子","小四川"]
 
 
 far_full_rice=["九龍燒臘"]
